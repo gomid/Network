6dbe6801a1bb51d5334fdce3c23bc6f4fdcb430d
QIJIA WANG <qwang000@gmail.com>
2018-09-02 10:24:44 +0800

    minor change

--- src/network.py
+++ src/network.py
@@ -1,5 +1,5 @@
 from layers import *
-from utils import *
+import utils
 import time
 
 
@@ -45,14 +45,14 @@ class NeuralNetwork:
         self.gradient_descent()
         return batch_loss, batch_acc
 
-    def train(self, X_train, y_train, X_val, y_val, iterations, batch_size, save_plot_to_directory):
+    def train(self, X_train, y_train, X_val, y_val, iterations, batch_size, save_plot_to_directory=None):
         start_time = time.time()
         train_loss = []
         train_acc = []
         val_loss = []
         val_acc = []
         for itr in range(iterations):
-            shuffled_X, shuffled_y = unison_shuffled_copies(X_train, y_train)
+            shuffled_X, shuffled_y = utils.unison_shuffled_copies(X_train, y_train)
             itr_loss = []
             itr_accuracy = []
             for i in range(0, len(shuffled_X), batch_size):
@@ -70,32 +70,31 @@ class NeuralNetwork:
             val_acc.append(acc)
 
         elapsed_time = time.time() - start_time
+        print("Accuracy: {0:.6f}s".format(val_acc[-1]))
         print("Time taken: {0:.2f}s".format(elapsed_time))
         if save_plot_to_directory:
-            plot_epoch(save_plot_to_directory,
-                       self.name,
-                       self.lr,
-                       batch_size,
-                       iterations,
-                       train_loss,
-                       train_acc,
-                       val_loss,
-                       val_acc,
-                       elapsed_time)
+            utils.plot_epoch(
+                save_plot_to_directory,
+                self.name,
+                self.lr,
+                batch_size,
+                iterations,
+                train_loss,
+                train_acc,
+                val_loss,
+                val_acc,
+                elapsed_time
+            )
 
     def test(self, X_test, y_test):
         loss, acc = self.forward_propagation(X_test, y_test)
         return loss, acc
 
     def export_gradients(self, directory):
-        with open(f"{directory}/dw-{self.name}.csv", "w+") as csv_file:
+        dw_filename = f"{directory}/dw-{self.name}.csv"
+        db_filename = f"{directory}/db-{self.name}.csv"
+        with open(dw_filename, "w+") as dw_file, open(db_filename, "w+") as db_file:
             for layer in self.layers:
                 if type(layer) is FullyConnectedLayer:
-                    np.savetxt(csv_file, layer.gW, fmt="%.6f", delimiter=",")
-        with open(f"{directory}/db-{self.name}.csv", "w+") as csv_file:
-            for layer in self.layers:
-                if type(layer) is FullyConnectedLayer:
-                    np.savetxt(csv_file, layer.gb, fmt="%.6f", delimiter=",")
-
-
-
+                    np.savetxt(dw_file, layer.gW, delimiter=",")
+                    np.savetxt(db_file, np.reshape(layer.gb, (1, layer.gb.shape[0])), delimiter=",")
